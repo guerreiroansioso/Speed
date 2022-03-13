01b3c6cd78f89d28ec10f74c88dc0eacd80a70fa
Felipe Augusto Cruz <felipeaugustocruz10@gmail.com>
2022-03-13 19:51:47 -0300

    Implementação UDP iniciado.

--- Assets/Conexao.py
+++ Assets/Conexao.py
@@ -10,6 +10,8 @@ class Conexao:
         self.udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.infoLocal = (ipLocal, int(porta))
         self.infoExterno = (ipExterno, int(porta))
+        self.udpLocal = (ipLocal, int(porta + 1))
+        self.udpExterno = (ipExterno, int(porta + 1))
 
     @staticmethod
     def Verificar(string):
@@ -19,21 +21,33 @@ class Conexao:
     def Apoio(self):
         self.tcp.bind(self.infoLocal)
         self.tcp.listen(1)
-        conexao, externo = self.tcp.accept()
-        Exibir.Correto('Recebeu conexão de ', conexao, '.')
+        self.udp.bind(self.udpLocal)
 
-        conteudoRecebido = (conexao.recv(64)).decode()
-        if conteudoRecebido == '[TCP]':
-            quantidadeTCP = 0
-            while True:
-                conteudoLoop = (conexao.recv(1024)).decode()
-                if conteudoLoop == '[/TCP]': break
-                if conteudoLoop is not None: quantidadeTCP += 1
+        while True:
+            conexao, externo = self.tcp.accept()
+            Exibir.Correto('Recebeu conexão de ', conexao, '.')
+
+            conteudoRecebido = (conexao.recv(64)).decode()
+            if conteudoRecebido == '[TCP]':
+                quantidadeTCP = 0
+                while True:
+                    conteudoLoop = (conexao.recv(64)).decode()
+                    if conteudoLoop == '[/TCP]': break
+                    if conteudoLoop is not None: quantidadeTCP += 1
+                
+                conexao.send(f'[{quantidadeTCP}]'.encode())
             
-            conexao.send(f'[{quantidadeTCP}]'.encode())
+            if conteudoRecebido == '[UDP]':
+                quantidadeUDP = 0
+                while True:
+                    conteudoLoop, cliente = self.udp.recvfrom(64)
+                    if conteudoLoop == '[/UDP]': break
+                    if conteudoLoop is not None: quantidadeUDP += 1
+                
+                conexao.send(f'[{quantidadeUDP}]'.encode())
 
-        time.sleep(1)
-        Conexao.Close(conexao)
+            time.sleep(1)
+            Conexao.Close(conexao)
 
 
     def Transferir(self):
@@ -44,9 +58,9 @@ class Conexao:
 
         time.sleep(1)
 
+        Exibir.Correto('Enviando pacotes TCP...')
         iterador = 0; tempoMaximo = 20; tempoInicial = time.time()
         while True:
-            Exibir.Correto('Enviando pacotes ...')
             tempoAtual = time.time()
             for variavel in range(8):
                 conteudoPreparado = f'[{iterador}]'.encode()
@@ -65,7 +79,37 @@ class Conexao:
         retornoSeparado = Conexao.Verificar(retornoApoio)
         Exibir.Correto('Último pacote enviado: ', retornoSeparado)
 
-        Conexao.Close(self.tcp)
+        #----- UDP
+        pedidoDeEnvio = '[UDP]'.encode()
+        self.udp.send(pedidoDeEnvio)
+
+        time.sleep(1)
+
+        Exibir.Correto('Enviando pacotes UDP...')
+        iterador = 0; tempoMaximo = 20; tempoInicial = time.time()
+        while True:
+            Exibir.Correto('Enviando pacotes ...')
+            tempoAtual = time.time()
+            for variavel in range(8):
+                conteudoPreparado = f'[{iterador}]'.encode()
+                
+                self.udp.sendto(conteudoPreparado, self.udpExterno)
+                iterador += 1
+            tempoCorrente = tempoAtual - tempoInicial
+            if tempoCorrente >= tempoMaximo:
+                break
+
+        self.tcp.send('[/UDP]'.encode())
+
+        time.sleep(1)
+
+        retornoApoio = (self.tcp.recv(64)).decode()
+        retornoSeparado = Conexao.Verificar(retornoApoio)
+        Exibir.Correto('Último pacote enviado: ', retornoSeparado)
+
+
+
+        #Conexao.Close(self.tcp)
         
     @staticmethod
     def Close(socket):
