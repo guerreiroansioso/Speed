027a711b4d0e9785ccb151e9c93b9d2fcb58d6d3
Felipe Augusto Cruz <felipeaugustocruz10@gmail.com>
2022-03-15 00:43:45 -0300

    Adaptação ds chamadas.

--- Assets/Conexao.py
+++ Assets/Conexao.py
@@ -18,49 +18,26 @@ class Conexao:
         resultadoRegEx = re.findall(r'\[(.+?)\]', string)
         return resultadoRegEx
 
-    def Apoio(self):
-        self.tcp.bind(self.infoLocal)
-        self.tcp.listen(1)
-        self.udp.bind(self.udpLocal)
-
+    def RedirecionarTCP(self):
+        quantidadeTCP = 0
         while True:
-            conexao, externo = self.tcp.accept()
-            Exibir.Correto('Recebeu conexão de ', conexao, '.')
+            conteudoLoop = (self.conexao.recv(64)).decode()
+            if conteudoLoop == '[/TCP]': break
+            if conteudoLoop is not None: quantidadeTCP += 1
+        
+        self.conexao.send(f'[{quantidadeTCP}]'.encode())
 
-            conteudoRecebido = (conexao.recv(64)).decode()
-            if conteudoRecebido == '[TCP]':
-                quantidadeTCP = 0
-                while True:
-                    conteudoLoop = (conexao.recv(64)).decode()
-                    if conteudoLoop == '[/TCP]': break
-                    if conteudoLoop is not None: quantidadeTCP += 1
-                
-                conexao.send(f'[{quantidadeTCP}]'.encode())
+    def RedirecionarUDP(self):
+        quantidadeUDP = 0
+        while True:
+            conteudoLoop, cliente = self.udp.recvfrom(64)
+            if conteudoLoop == '[/UDP]': break
+            if conteudoLoop is not None: quantidadeUDP += 1
             
-            if conteudoRecebido == '[UDP]':
-                conexao.send('[*UDP]'.encode())
-                quantidadeUDP = 0
-                while True:
-                    conteudoLoop, cliente = self.udp.recvfrom(64)
-                    if conteudoLoop == '[/UDP]': break
-                    if conteudoLoop is not None: quantidadeUDP += 1
-                
-                conexao.send(f'[{quantidadeUDP}]'.encode())
+        self.conexao.send(f'[{quantidadeUDP}]'.encode())
 
-            time.sleep(1)
-            #Conexao.Close(conexao)
-
-
-    def Transferir(self):
-        self.tcp.connect(self.infoExterno)
-
-        pedidoDeEnvio = '[TCP]'.encode()
-        self.tcp.send(pedidoDeEnvio)
-
-        time.sleep(1)
-
-        Exibir.Correto('Enviando pacotes TCP...')
-        iterador = 0; tempoMaximo = 20; tempoInicial = time.time()
+    def EnviarTCP(self, tempoMaximo):
+        iterador = 0; tempoInicial = time.time()
         while True:
             tempoAtual = time.time()
             for variavel in range(8):
@@ -74,45 +51,41 @@ class Conexao:
 
         self.tcp.send('[/TCP]'.encode())
 
+    def Apoio(self):
+        self.tcp.bind(self.infoLocal)
+        self.tcp.listen(1)
+        self.udp.bind(self.udpLocal)
+
+        while True:
+            self.conexao, externo = self.tcp.accept()
+            Exibir.Correto('Recebeu conexão de ', self.conexao, '.')
+
+            conteudoRecebido = (self.conexao.recv(64)).decode()
+            if conteudoRecebido == '[TCP]':
+                self.RedirecionarTCP()
+            
+            if conteudoRecebido == '[UDP]':
+                self.RedirecionarUDP()
+
+            time.sleep(1)
+            #Conexao.Close(conexao)
+
+
+    def Transferir(self):
+        self.tcp.connect(self.infoExterno)
+
+        pedidoDeEnvio = '[TCP]'.encode()
+        self.tcp.send(pedidoDeEnvio)
+
         time.sleep(1)
+
+        Exibir.Simples('Enviando pacotes TCP...')
+        self.EnviarTCP(5)
 
         retornoApoio = (self.tcp.recv(64)).decode()
         retornoSeparado = Conexao.Verificar(retornoApoio)
-        Exibir.Correto('Último pacote enviado: ', retornoSeparado)
-
-        #----- UDP
-        while True:
-            pedidoDeEnvio = '[UDP]'.encode()
-            self.tcp.send(pedidoDeEnvio)
-            udpRetorno = (self.tcp.recv(64)).decode()
-            if udpRetorno == '[*UDP]': break
+        Exibir.Simples('Último pacote enviado: ', retornoSeparado)
         
-
-        time.sleep(1)
-
-        Exibir.Correto('Enviando pacotes UDP...')
-        iterador = 0; tempoMaximo = 20; tempoInicial = time.time()
-        while True:
-            Exibir.Correto('Enviando pacotes ...')
-            tempoAtual = time.time()
-            for variavel in range(8):
-                conteudoPreparado = f'[{iterador}]'.encode()
-                
-                self.udp.sendto(conteudoPreparado, self.udpExterno)
-                iterador += 1
-            tempoCorrente = tempoAtual - tempoInicial
-            if tempoCorrente >= tempoMaximo:
-                break
-
-        self.tcp.send('[/UDP]'.encode())
-
-        time.sleep(1)
-
-        retornoApoio = (self.tcp.recv(64)).decode()
-        retornoSeparado = Conexao.Verificar(retornoApoio)
-        Exibir.Correto('Último pacote enviado: ', retornoSeparado)
-
-
 
         #Conexao.Close(self.tcp)
         
